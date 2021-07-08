1274bceea8d6f1c8651318e32b7a52581fbbd9e2
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-07-08 18:22:05 +0200

    Adding new Apartment - creation of an ApartmentApplicationService

---
+++ src/main/java/com/bluesoft/rentalapplication/domain/apartment/Apartment.java
@@ -0,0 +1,17 @@
+package com.bluesoft.rentalapplication.domain.apartment;
+
+import java.util.List;
+
+public class Apartment {
+    private final String ownerId;
+    private final Address address;
+    private final String description;
+    private final List<Room> rooms;
+
+    public Apartment(final String ownerId, final Address address, final String description, final List<Room> rooms) {
+        this.ownerId = ownerId;
+        this.address = address;
+        this.description = description;
+        this.rooms = rooms;
+    }
+}
