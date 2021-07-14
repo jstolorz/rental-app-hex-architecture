d2f129f6cbf1a317ab5b5dce1aae154047377f41
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-07-14 12:40:54 +0200

    Book an Apartment - add getters to ApartmentBooked and use LocalDate instead of Period

--- src/main/java/com/bluesoft/rentalapplication/domain/apartment/ApartmentBooked.java
+++ src/main/java/com/bluesoft/rentalapplication/domain/apartment/ApartmentBooked.java
@@ -1,32 +1,64 @@
 package com.bluesoft.rentalapplication.domain.apartment;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-class ApartmentBooked {
+public class ApartmentBooked {
 
 
     private final String eventId;
-    private final Object eventCreationDateTime;
+    private final LocalDateTime eventCreationDateTime;
     private final String apartmemtId;
     private final String ownerId;
     private final String tenantId;
-    private final Period period;
+    private final LocalDate periodStart;
+    private final LocalDate periodEnd;
 
-    ApartmentBooked(final String eventId, final Object eventCreationDateTime, final String apartmemtId, final String ownerId, final String tenantId, final Period period) {
+
+    ApartmentBooked(final String eventId, final LocalDateTime eventCreationDateTime, final String apartmemtId, final String ownerId, final String tenantId, final Period period) {
 
         this.eventId = eventId;
         this.eventCreationDateTime = eventCreationDateTime;
         this.apartmemtId = apartmemtId;
         this.ownerId = ownerId;
         this.tenantId = tenantId;
-        this.period = period;
+        periodStart = period.getStart();
+        periodEnd = period.getEnd();
     }
 
     static ApartmentBooked create(String apartmemtId, String ownerId, String tenantId, Period period){
         final String eventId = UUID.randomUUID().toString();
-        final Object eventCreationDateTime = LocalDateTime.now();
+        final LocalDateTime eventCreationDateTime = LocalDateTime.now();
 
         return new ApartmentBooked(eventId, eventCreationDateTime, apartmemtId, ownerId, tenantId, period);
+    }
+
+   public String getEventId() {
+        return eventId;
+    }
+
+   public LocalDateTime getEventCreationDateTime() {
+        return eventCreationDateTime;
+    }
+
+   public String getApartmemtId() {
+        return apartmemtId;
+    }
+
+   public String getOwnerId() {
+        return ownerId;
+    }
+
+   public String getTenantId() {
+        return tenantId;
+    }
+
+   public LocalDate getPeriodStart() {
+        return periodStart;
+    }
+
+   public LocalDate getPeriodEnd() {
+        return periodEnd;
     }
 }
